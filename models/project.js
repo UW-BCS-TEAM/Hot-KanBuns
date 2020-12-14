7ebda59fc666502493d26eb6101d38be43bc8a69
Bailey Clemons <baileythevampireslayer@gmail.com>
2020-12-14 13:44:50 -0800

    "fixing routes"

--- models/project.js
+++ models/project.js
@@ -1,4 +1,3 @@
-const author = require("../../../webdev/uw-sea-fsf-pt-03-2020-u-c/14-Full-Stack/01-Activities/13-Post-Author-Association/Solved/models/author");
 
 module.exports = function(sequelize, DataTypes) {
     const Project = sequelize.define("Project", {
