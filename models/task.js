b914ce29111b3aa010361f8b4f84292c9a4f33f2
Scott Nelson <snelson.seattle@gmail.com>
2020-12-14 17:16:06 -0800

    added projectId field to task.js sequelize model

--- models/task.js
+++ models/task.js
@@ -17,6 +17,10 @@ module.exports = function(sequelize, DataTypes){
             type: DataTypes.STRING,
             allowNull: false,
             defaultValue: "low"
+        },
+        projectId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
     });
 
