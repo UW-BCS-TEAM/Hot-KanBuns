0ead1b7d58b3eebc93d72f70725a37f917a9beb4
Bailey Clemons <baileythevampireslayer@gmail.com>
2020-12-17 21:31:24 -0800

    "working on routes"

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
-var mysql = require("mysql");
+const app = express()
+var exphbs = require("express-handlebars");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Import the Sequelize models
 const db = require("../models");
@@ -10,15 +17,15 @@ const db = require("../models");
 // ----------------------------------
 router.get("/api/projects/:userID?", (req, res) => {
     // Check for user authentication before making query
-    if(!req.user){
-        res.json({Error: "Unauthorized User"});
+    if (!req.user) {
+        res.json({ Error: "Unauthorized User" });
     }
-    if(req.params.userID){
-        db.Project.findAll({where: {userid: req.params.userID}}).then(projectData => {
+    if (req.params.userID) {
+        db.Project.findAll({ where: { userid: req.params.userID } }).then(projectData => {
             res.json(projectData);
         });
     }
-    else{
+    else {
         db.Project.findAll({}).then(projectData => {
             res.json(projectData);
         });
@@ -27,24 +34,24 @@ router.get("/api/projects/:userID?", (req, res) => {
 
 router.post("/api/projects/:userID", (req, res) => {
     // Check for user authentication before making query
-    if(!req.user){
-        res.json({Error: "Unauthorized User"});
-    }else{
+    if (!req.user) {
+        res.json({ Error: "Unauthorized User" });
+    } else {
         db.Project.create({
             projectName: req.body.projectName,
             projectDesc: req.body.projectDesc,
             userId: parseInt(req.params.userID)
-          }).then(result => {
-              res.json({ id: result.insertId });
-          });
+        }).then(result => {
+            res.json({ id: result.insertId });
+        });
     }
 });
 
 router.put("/api/projects/:projectID", (req, res) => {
     // Check for user authentication before making query
-    if(!req.user){
-        res.json({Error: "Unauthorized User"});
-    }else{
+    if (!req.user) {
+        res.json({ Error: "Unauthorized User" });
+    } else {
         db.Project.update({
             projectName: req.body.projectName,
             projectDesc: req.body.projectDesc
@@ -60,9 +67,9 @@ router.put("/api/projects/:projectID", (req, res) => {
 
 router.delete("/api/projects/:projectID", (req, res) => {
     // Check for user authentication before making query
-    if(!req.user){
-        res.json({Error: "Unauthorized User"});
-    }else{
+    if (!req.user) {
+        res.json({ Error: "Unauthorized User" });
+    } else {
         db.Project.destroy({
             where: {
                 id: req.params.projectID
@@ -73,19 +80,36 @@ router.delete("/api/projects/:projectID", (req, res) => {
     }
 });
 
-app.get("/", function(req, res) {
-    //SELECT * FROM projects
 
-    connection.query("SELECT * FROM projects;", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
 
-      res.render('members', {projects: data})
-      //res.json(data);
+app.get("/", function (req, res) {
+
+
+    connection.query("SELECT * FROM projects;", function (err, data) {
+        if (err) {
+            return res.status(500).end();
+        }
+
+        res.render('members', { projects: data })
+
     });
 
-  });
+});
+
+// app.get("/api/projects/:userID", function(req, res) {
+
+//     connection.query("SELECT * FROM projects where UserId = ?", [req.params.id], function(err, data) {
+//       if (err) {
+//         return res.status(500).end();
+//       }
+
+//       console.log(data);
+//       res.render('members', { projects: data })
+//     });
+//   });
+
+
+
 
 
 // Export the router
--- public/js/members.js
+++ public/js/members.js
@@ -1,10 +1,78 @@
-$(document).ready(function() {
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    $.get("/api/user_data").then(function(data) {
-      $(".member-name").text(data.email);
-    });
+// $(document).ready(function() {
+//     // This file just does a GET request to figure out which user is logged in
+//     // and updates the HTML on the page
+//     $.get("/api/user_data").then(function(data) {
+//       $(".member-name").text(data.email);
+//     });
 
-    
 
-    });
+
+//     });
+
+$(document).ready(function () {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+
+  let userId = null;
+
+  $.get("/api/user_data").then(function (data) {
+    $(".member-name").text(data.email);
+    userId = data.id;
+  });
+
+
+
+  $.get("/api/projects").then(function (data) {
+    console.log("data", data);
+    for (i = 0; i < 2;) {
+      const allProjectsEl = $("#all-projects");
+      const listEl = $("<li>");
+
+
+
+      listEl.text(data[i].projectName);
+
+      allProjectsEl.append(listEl);
+
+      i++
+    }
+
+  });
+
+
+
+  $.get("/api/projects").then(function (data) {
+    for (i = 0; i < 3;) {
+      const myProjectsEl = $("#my-projects");
+      const listEl = $("<li>");
+
+      listEl.text(data[i].projectName);
+
+      myProjectsEl.append(listEl);
+
+      i++
+    }
+  });
+
+
+  //New Project
+  $("#create-btn").on("click", function (event) {
+    event.preventDefault();
+
+
+
+    const newProj = {
+      projectName: $("#name").val().trim(),
+      projectDesc: $("#desc").val().trim()
+    };
+    console.log("new project", newProj)
+
+    $.post(`/api/projects/${userId}`, newProj).then(function() {
+      console.log("created new project");
+
+      // location.reload();
+    }
+    );
+  });
+});
+
