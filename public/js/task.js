d58708416e1b363b9d1a912b0b27c7efdf246208
Ion Gavrilita <gavrilita.ion.93@gmail.com>
2020-12-17 21:23:13 -0800

    create handlebars tasks

--- public/js/task.js
+++ public/js/task.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     var formtask = $("form.create-form")
     var taskName= $("input#taskname");
@@ -9,17 +10,11 @@ formtask.on("submit", function(event){
     event.preventDefault();
     var taskData = {
         name: taskName.val().trim(),
-        description:taskDescr.val().trim(),
+        description:taskDesc.val().trim(),
         level: taskPriority.val().trim(),
         progress:  taskStatus.val().trim()
     }
 
-
-// taskUser(taskData.name, taskData.description, taskData.level, taskData.progress);
-// taskName.val("");
-// taskDesc.val("");
-// taskPriority.val("");
-// taskStatus.val("");
    
    // Send the POST request.
    $.ajax("/api/tasks/1", {
@@ -33,8 +28,39 @@ formtask.on("submit", function(event){
     }
   );
 
-
-
     })
 })
+
+
+$(function () {
+    var str = "",
+        inHTML = ""
+    $.get("/api/users",function(data) {
+      //console.log(data);
+       $.each(data, function (i, ob) {
+        inHTML += '<option value="' + ob.id + '">' + ob.firstName + ' ' + ob.lastName + '</option>';
+    });
+    $("#AllUsers").empty().append(inHTML);
+    }); 
+   $.get("/api/users/2", function(data1) {
+     inHTML = "";
+     $.each(data1, function (i, ob) {
+        inHTML += '<option value="' + ob.id + '">' + ob.firstName + ' ' + ob.lastName + '</option>';
+    });
+    $("#TaskUsers").empty().append(inHTML);
+   });
+$('#add').click(function () {
+    inHTML = "";
+
+    $("#AllUsers option:selected").each(function () {
+        if ($("#TaskUsers option[value=" + $(this).val() + "]").length == 0) {
+            inHTML += '<option value="' + $(this).val() + '">' + $(this).text() + '</option>';
+        }
+    });
+    $("#TaskUsers").append(inHTML);
+});
+$('#remove').click(function () {
+    $('#TaskUsers option:selected').remove();
+});
+});
 
